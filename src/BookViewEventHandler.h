092873c5d4474100f5a1457a95340ee2590a595c
Jason Turner <lefticus@gmail.com>
2003-04-03 18:08:39 +0000

    Initial checkin

---
+++ src/BookViewEventHandler.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Jason Turner                                    *
+ *   jason@whensdinner.com                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef _BOOKVIEWEVENTHANDLER_H_
+#define _BOOKVIEWEVENTHANDLER_H_
+
+#include "biblestudy.h"
+
+class BookViewEventHandler;
+
+#include <wx/event.h>
+#include "BookViewCtrl.h"
+
+
+/**
+ * 
+ * Jason Turner
+ **/
+class BookViewEventHandler : public wxEvtHandler
+{
+private:
+	BookViewCtrl *m_parent;
+
+public:
+	BookViewEventHandler();
+	~BookViewEventHandler();
+  
+	void OnSetFocus(wxFocusEvent &event);
+	void SetParent(BookViewCtrl *parent);
+	
+	DECLARE_EVENT_TABLE()  
+	
+	
+};
+
+#endif
